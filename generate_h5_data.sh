b73924e3058c1ae8f837a9eae22671844324564b
Rajat Sharma <rjt.rjtshrm94@gmail.com>
2021-06-14 09:39:56 +0200

    Uncomment lines in generate_h5_data.sh

--- generate_h5_data.sh
+++ generate_h5_data.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#train_list="modelnet_list/train.txt"
-#test_list="modelnet_list/test.txt"
-#val_list="modelnet_list/val.txt"
+train_list="modelnet_list/train.txt"
+test_list="modelnet_list/test.txt"
+val_list="modelnet_list/val.txt"
 
 output_root_path_h5=$1 # path where to store depth off files
 n_views=$2 # number of views
 output_root_path_depth=$3 # path where to save depth maps for u-net
 
-#rm -rf $output_root_path_h5 # if already exists
-#mkdir -p $output_root_path_h5
-#mkdir -p $output_root_path_h5/train
-#mkdir -p $output_root_path_h5/test
-#mkdir -p $output_root_path_h5/val
+rm -rf $output_root_path_h5 # if already exists
+mkdir -p $output_root_path_h5
+mkdir -p $output_root_path_h5/train
+mkdir -p $output_root_path_h5/test
+mkdir -p $output_root_path_h5/val
 
 rm -rf $output_root_path_depth # if already exists
 mkdir -p $output_root_path_depth
@@ -22,11 +22,11 @@ mkdir -p $output_root_path_depth/val
 
 # create data for training
 out_path=$output_root_path_h5/train
-#while read line; do
+while read line; do
 ##Reading each line
-#echo 'Creating depthmaps for train file', $line
-#python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
-#done < $train_list
+echo 'Creating depthmaps for train file', $line
+python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
+done < $train_list
 
 # create raw depth images to train u-net for the depth routing
 # training
@@ -34,11 +34,11 @@ python generate_depth_from_h5.py --in_h5_file_folder $out_path --out_depth_path 
 
 # create data for testing
 out_path=$output_root_path_h5/test
-#while read line; do
+while read line; do
 ##Reading each line
-#echo 'Creating depthmaps for test file', $line
-#python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
-#done < $test_list
+echo 'Creating depthmaps for test file', $line
+python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
+done < $test_list
 
 # create raw depth images to train u-net for the depth routing
 # testing
@@ -46,11 +46,11 @@ python generate_depth_from_h5.py --in_h5_file_folder $out_path --out_depth_path 
 
 # create data for validation
 out_path=$output_root_path_h5/val
-#while read line; do
+while read line; do
 ##Reading each line
-#echo 'Creating depthmaps for val file', $line
-#python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
-#done < $val_list
+echo 'Creating depthmaps for val file', $line
+python render_depth_from_modelnet.py --in_file $line --out_path $out_path --n_views $n_views
+done < $val_list
 
 # create raw depth images to train u-net for the depth routing
 # validation
